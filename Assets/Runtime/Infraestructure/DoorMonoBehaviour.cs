3d8a2abec63364de1534da498464c6c373d9ce04
Papajoshh <josecarlosromerodiaz@gmail.com>
2026-01-30 18:11:51 +0100

    feat: updated doors

--- Assets/Runtime/Infraestructure/DoorMonoBehaviour.cs
+++ Assets/Runtime/Infraestructure/DoorMonoBehaviour.cs
@@ -5,11 +5,12 @@ namespace Runtime.Infraestructure
 {
     public class DoorMonoBehaviour : MonoBehaviour, Door
     {
-        [SerializeField] private Transform destination;
+        [SerializeField] private Transform entrancePosition;
         [SerializeField] private SpriteRenderer spriteRenderer;
         [SerializeField] private Sprite openSprite;
         [SerializeField] private Sprite closedSprite;
         [SerializeField] private Collider2D triggerCollider;
+        [SerializeField] private DoorMonoBehaviour destinationDoor;
         [SerializeField] private RoomMonoBehaviour destinationRoom;
 
         [SerializeField] private bool startClosed = true;
@@ -42,8 +43,10 @@ namespace Runtime.Infraestructure
         public void CrossTo(Transform objectToMove)
         {
             if (!IsOpened) return;
-            objectToMove.position = destination.position;
+            objectToMove.position = destinationDoor.GetEntrancePosition();
             if (destinationRoom) destinationRoom.TryDiscover();
         }
+
+        public Vector3 GetEntrancePosition() => entrancePosition.position;
     }
 }
